890be03a234728e11f7013d99fcb2f2a41a058b7
Alex Drahin <dragin.alex@gmail.com>
2024-02-27 19:36:39 +0100

    task max-in-seq

---
+++ max-in-seq2/src/FindMaxInSeq.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class FindMaxInSeq {
+    public static int max() {
+
+        // Your code is here
+
+
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Test your code here!\n");
+
+        // Get a result of your code
+
+        System.out.println(max());
+    }
+}
